a0c273ce9961d480392f07c9d4b072f0ebcdf331
aswathikp758 <aswathikp758@gmail.com>
2023-07-26 15:27:06 +0530

    update

--- cart/backend/index.js
+++ cart/backend/index.js
@@ -14,12 +14,12 @@ app.use(express.json({limit:"10mb"}));
 const PORT=process.env.PORT||8080;
 
 //static file
-// if (process.env.NODE_ENV === "production") {
-//    app.use(express.static("frontend/build"));
-//    app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//    });
-// }
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("frontend/build"));
+   app.get("/", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+   });
+}
 
 
 //mongodb connection
