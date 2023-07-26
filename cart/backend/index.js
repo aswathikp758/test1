346b402792880d81c3c47276ea1cafc49891e54a
aswathikp758 <aswathikp758@gmail.com>
2023-07-26 15:40:53 +0530

    updation

--- cart/backend/index.js
+++ cart/backend/index.js
@@ -10,16 +10,25 @@ const Stripe = require('stripe');
 const app=express();
 app.use(cors());
 app.use(express.json({limit:"10mb"}));
+app.use(express.static(path.join(__dirname,'./frontend/build')))
+
+//rest api
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
+})
+
+
+
 
 const PORT=process.env.PORT||8080;
 
 //static file
-if (process.env.NODE_ENV === "production") {
-   app.use(express.static("frontend/build"));
-   app.get("/", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-   });
-}
+// if (process.env.NODE_ENV === "production") {
+//    app.use(express.static("frontend/build"));
+//    app.get("/", (req, res) => {
+//       res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//    });
+// }
 
 
 //mongodb connection
